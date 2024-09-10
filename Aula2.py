25eb1cbd01635ddec06dc0766f015112ec6dab3f
João Pedro Araujo Queiroz Barbosa <111623525+joaopedroaqb@users.noreply.github.com>
2024-09-09 23:35:38 -0300

    Update Aula2.py

--- Aula2.py
+++ Aula2.py
@@ -53,3 +53,72 @@ primeiro_nome = "João"
 sobrenome = "Pedro"
 nome_completo = primeiro_nome + " " + sobrenome
 print(nome_completo)
+
+
+
+
+
+# CODIGO JOÃO PEDRO
+
+
+print("Exercicio 1: 'nome'\nExercicio 2: 'idade'\nExercicio 3: 'A e B'\nExercicio 4: 'Cidade'\nExercicio 5: 'X e Y'\nExercicio 6: 'pi'\nExercicio 7: 'mensagem'\nExercicio 8: 'Base x Altura'\nExercicio 9: 'saldo'\nExercicio 10: 'nome completo' ")
+opc = int(input("Digite qual Exercicio você quer: "))
+
+if opc == 1:
+    print("Você escolheu opção 1")
+    nome = str(input("Digite seu nome: "))
+    print("Seu nome é {}".format(nome))
+
+elif opc == 2:
+    print("Você escolheu opção 2")
+    idade = int(input("Digite sua idade: "))
+    print("Sua idade é: ", idade, "anos")
+
+elif opc == 3:
+    print("Você escolheu opção 3")
+    a = int(input("Digite o valor de 'a': "))
+    b = int(input("Digite o valor de 'b': "))
+    print("A soma de a + b = {}".format(a+b))
+
+elif opc == 4:
+    print("Você escolheu opção 4")
+    cidade = str(input("Digite o nome de uma cidade: "))
+    print("A cidade que você digitou foi '{}'".format(cidade))
+
+elif opc == 5:
+    print("Você escolheu opção 5")
+    x = int(input("Digite o valor de 'x': "))
+    y = int(input("Digite o valor de 'y': "))
+    print("A soma de a + b = {}".format(x-y))
+
+elif opc == 6:
+    print("Você escolheu opção 6")
+    pi = 3.14159
+    multi = int(input("Escolha a quantidade de vezes que você quer multiplicar 'pi': "))
+    print("{} x pi = {}".format(multi, multi*pi))
+
+elif opc == 7:
+    print("Você escolheu opção 7")
+    mensagem = input("Escreva uma frase para ser repetida 3x: ")
+    print(mensagem*3)
+
+elif opc == 8:
+    print("Você escolheu opção 8")
+    base = float(input("Digite o valor da base do retangulo: "))
+    alt = float(input("Digite o valor da altura do retangulo: "))
+    print("A area do retangulo é de: {}".format(base*alt))
+
+elif opc == 9:
+    print("Você escolheu opção 9")
+    saldo = float(input("Digite o saldo atual: "))
+    retirada = float(input("Digite o valor da retirada que deseja fazer: "))
+    print("Seu saldo é de {}, você deseja retirar {}, o saldo final ficará de {} reais".format(saldo, retirada, saldo-retirada))
+
+elif opc == 10:
+    print("Você escolheu opção 10")
+    nome1 = str(input("Digite seu nome: "))
+    sobrenome = str(input("Digite seu sobrenome: "))
+    print("Seu nome completo é: {}".format(nome1 + sobrenome ))
+
+else:
+    print("Opção invalida, escolha uma opção correta")
